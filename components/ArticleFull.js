71045452b47149bf7963cf586c5806ebfce54f4a
Aawaz <awazgyawali@gmail.com>
2018-09-19 12:49:15 +0545

    Featured image in full article

--- components/ArticleFull.js
+++ components/ArticleFull.js
@@ -1,7 +1,22 @@
 import React, { PureComponent } from 'react'
 import renderHTML from 'react-render-html'
+import { getDownloadUrl } from '../helpers/firebase'
 
 export default class ArticleFull extends PureComponent {
+  state = {
+    image_url: ''
+  }
+
+  componentDidMount () {
+    this.getImageUrl(this.props.blog.image_reference)
+  }
+
+  async getImageUrl (image_ref) {
+    this.setState({
+      image_url: await getDownloadUrl(image_ref)
+    })
+  }
+
   render () {
     return (
       <div
@@ -10,10 +25,16 @@ export default class ArticleFull extends PureComponent {
           margin: '20px'
         }}
       >
-        <img src={this.props.imageUrl} />
         <h1>{this.props.blog.title}</h1>
+        <img src={this.state.image_url} style={styles.featuredImage} />
         <p>{renderHTML(this.props.blog.message)}</p>
       </div>
     )
+  }
+}
+
+const styles = {
+  featuredImage: {
+    widht: '100%',
   }
 }
