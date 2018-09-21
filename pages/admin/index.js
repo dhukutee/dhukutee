5b2dfd076d4fbd9838f287448445464d728432f6
Aawaz <awazgyawali@gmail.com>
2018-09-21 18:05:18 +0545

    /admin route protected

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,47 +1,51 @@
 import Router from "next/router";
 
-import {
-  getAllBlog,
-  getDownloadUrl,
-  deleteABlog
-} from "../../helpers/firebase";
+import { getAllBlog, isLoggedIn, deleteABlog } from "../../helpers/firebase";
 import MainLayout from "../../layouts/MainLayout";
 import ArticleTile from "../../components/ArticleTile";
 
-const AdminPage = props => (
-  <MainLayout>
-    <button
-      onClick={() => {
-        Router.push("/admin/write");
-      }}
-    >
-      Write a new Blog
-    </button>
-    <ul className="choices">
-      {props.blogs.map(blog => (
-        <div className="adminList" key={blog.url}>
-          <ArticleTile blog={blog} />
-          <button
-            onClick={() => {
-              Router.push(`/admin/write/${blog.url}`);
-            }}
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => {
-              deleteABlog(blog.url).then(() => {
-                Router.push(`/admin/`);
-              });
-            }}
-          >
-            Delete
-          </button>
-        </div>
-      ))}
-    </ul>
-  </MainLayout>
-);
+const AdminPage = props => {
+  isLoggedIn(user => {
+    if (!user) {
+      Router.push("/admin/login");
+    }
+  });
+
+  return (
+    <MainLayout>
+      <button
+        onClick={() => {
+          Router.push("/admin/write");
+        }}
+      >
+        Write a new Blog
+      </button>
+      <ul className="choices">
+        {props.blogs.map(blog => (
+          <div className="adminList" key={blog.url}>
+            <ArticleTile blog={blog} />
+            <button
+              onClick={() => {
+                Router.push(`/admin/write/${blog.url}`);
+              }}
+            >
+              Edit
+            </button>
+            <button
+              onClick={() => {
+                deleteABlog(blog.url).then(() => {
+                  Router.push(`/admin/`);
+                });
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </ul>
+    </MainLayout>
+  );
+};
 
 AdminPage.getInitialProps = async () => {
   // fetching the blog data from database before rendering the screen
